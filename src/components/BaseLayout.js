8dc642aeb27ea2f7e67e26005e3f61995884648b
ericareven <erica.reven@gmail.com>
2023-07-13 12:16:39 -0500

    removed eval

--- src/components/BaseLayout.js
+++ src/components/BaseLayout.js
@@ -1,54 +1,52 @@
-import React, {useEffect, useState} from 'react';
-import Style from './BaseLayout.module.scss'
+import React, { useEffect, useState } from 'react';
+import Style from './BaseLayout.module.scss';
 import Navbar from "./Navbar";
 import Home from "./home/Home";
 import About from "./about/About";
 import Projects from "./projects/Projects";
-import {Route, Routes} from "react-router-dom";
-import {Box, Grid} from "@mui/material";
+import { Route, Routes } from "react-router-dom";
+import { Box, Grid } from "@mui/material";
 
 export default function BaseLayout() {
    let [darkMode, setDarkMode] = useState(false);
 
    function handleToggleDarkMode() {
-      let oppositeOfCurrentDarkMode = !darkMode
-      console.log(oppositeOfCurrentDarkMode)
-      localStorage.setItem('darkMode', `${oppositeOfCurrentDarkMode}`)
-      setDarkMode(oppositeOfCurrentDarkMode)
+      let oppositeOfCurrentDarkMode = !darkMode;
+      console.log(oppositeOfCurrentDarkMode);
+      localStorage.setItem('darkMode', `${oppositeOfCurrentDarkMode}`);
+      setDarkMode(oppositeOfCurrentDarkMode);
    }
 
    useEffect(() => {
-      let detectedDarkMode = eval(localStorage.getItem('darkMode'));
+      let detectedDarkMode = localStorage.getItem('darkMode') === 'true';
 
       if (detectedDarkMode) {
-         setDarkMode(detectedDarkMode)
+         setDarkMode(detectedDarkMode);
       } else {
-         localStorage.setItem('darkMode', 'false')
+         localStorage.setItem('darkMode', 'false');
       }
-   }, [])
+   }, []);
 
    return (
       <Box className={darkMode ? Style.dark : Style.light}>
-         <Grid container display={'flex'} flexDirection={'column'} minHeight={'100vh'}
-               justifyContent={'space-between'}>
+         <Grid container display={'flex'} flexDirection={'column'} minHeight={'100vh'} justifyContent={'space-between'}>
             <Grid item>
-               <Navbar darkMode={darkMode} handleClick={handleToggleDarkMode}/>
+               <Navbar darkMode={darkMode} handleClick={handleToggleDarkMode} />
             </Grid>
             <Grid item flexGrow={1}>
                <Routes>
-                  <Route exact path={'/'} element={<Home/>}/>
-                  <Route exact path={'/about'} element={<About/>}/>
-                  <Route exact path={'/projects'} element={<Projects/>}/>
+                  <Route exact path={'/'} element={<Home />} />
+                  <Route exact path={'/about'} element={<About />} />
+                  <Route exact path={'/projects'} element={<Projects />} />
                </Routes>
             </Grid>
             <Grid item>
-               <Box component={'footer'} display={'flex'} flexDirection={'column'} alignItems={'center'}
-                    py={'1.5rem'} sx={{opacity: 0.7}} width={'100%'}>
+               <Box component={'footer'} display={'flex'} flexDirection={'column'} alignItems={'center'} py={'1.5rem'} sx={{ opacity: 0.7 }} width={'100%'}>
                   <p>&copy; 2023</p>
                </Box>
             </Grid>
          </Grid>
       </Box>
-   )
+   );
 }
 
